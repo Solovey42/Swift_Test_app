8e41df8c03398e43626f0cfc33858762429ad76f
Solovey42 <solovey.1999@mail.ru>
2020-10-11 03:50:42 +0700

    Completed fourth task

--- Sources/Swift_Test_app4/main.swift
+++ Sources/Swift_Test_app4/main.swift
@@ -3,3 +3,27 @@
 //
 
 import Foundation
+
+if CommandLine.arguments.count == 1 {
+    let StrIp = readLine()!
+    let SplitIp = StrIp.split(separator: ".")
+    CheckIp(SplitIp: SplitIp)
+} else {
+    let StrIp = CommandLine.arguments[1]
+    let SplitIp = StrIp.split(separator: ".")
+    CheckIp(SplitIp: SplitIp)
+
+}
+func CheckIp(SplitIp:[Substring]){
+    if(SplitIp.count==4){
+        for item in SplitIp{
+            if (Int(item) ?? -1 > 0 && Int(item) ?? -1 < 255){
+                continue
+            }
+        }
+        print("YES")
+    }
+    else{
+        print("NO")
+    }
+}
