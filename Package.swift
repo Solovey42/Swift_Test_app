46fc78d732ed76f869ec2ee89e7699ccae091db7
Solovey42 <solovey.1999@mail.ru>
2020-10-10 22:41:35 +0700

    Added testTargets in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift_Test_app1",
+    name: "Swift_Test_app",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -13,13 +13,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Swift_Test_app",
+            name: "Swift_Test_app1",
             dependencies: []),
         .target(
                 name: "Swift_Test_app2",
                 dependencies: []),
         .testTarget(
-            name: "Swift_Test_appTests",
-            dependencies: ["Swift_Test_app"]),
+            name: "Swift_Test_appTests1",
+            dependencies: ["Swift_Test_app1"]),
+        .testTarget(
+                name: "Swift_Test_appTests2",
+                dependencies: ["Swift_Test_app2"]),
     ]
 )
