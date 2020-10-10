bc8a0ec2015dc05fb3b9c3c00b8ae06d1d097662
Solovey42 <solovey.1999@mail.ru>
2020-10-10 22:40:21 +0700

    Completed second task

--- Sources/Swift_Test_app2/main.swift
+++ Sources/Swift_Test_app2/main.swift
@@ -3,12 +3,17 @@
 //
 
 import Foundation
+
 if CommandLine.arguments.count == 1 {
-    guard let name = readLine(strippingNewline: true) else {
-        exit(0)
-    }
+    let Speed = readLine()! //n
+    let Time = readLine()! //m
+    GetDist(Speed: Speed, Time: Time)
+} else {
+    let Speed = CommandLine.arguments[1] //n
+    let Time = CommandLine.arguments[2] //m
+    GetDist(Speed: Speed, Time: Time)
 }
-func say(name: String) {
-    let name = CommandLine.arguments[1]
-    print("Helloooooooooooooo, \(name)!")
+
+func GetDist(Speed: String, Time: String) {
+    print((109 + (Int(Speed)! * Int(Time)!) % 109) % 109)
 }
