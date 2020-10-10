ebf1e0ff76f70271c19a432dce9936bc4ac84570
Solovey42 <solovey.1999@mail.ru>
2020-10-11 01:37:59 +0700

    Completed third task

--- Sources/Swift_Test_app3/main.swift
+++ Sources/Swift_Test_app3/main.swift
@@ -3,3 +3,38 @@
 //
 
 import Foundation
+
+if CommandLine.arguments.count == 1 {
+    let String = readLine()!
+    var StringArray = [String]
+    var StringOut:String = ""
+    StringArray = String.components(separatedBy: " ")
+    distinct(StringOut: &StringOut,source: &StringArray)
+
+} else {
+    let Count = CommandLine.arguments[1]
+    var StringArray = [String]()
+    var StringOut:String = ""
+    for i in 2...1+Int(Count)!{
+        let Element = CommandLine.arguments[i]
+        StringArray.append(Element)
+    }
+    distinct(StringOut: &StringOut,source: &StringArray)
+
+}
+func distinct(StringOut: inout String,source: inout [String]) {
+    var unique = [String]()
+    for i in 0...source.count-1  {
+        let a = source[i]
+        source.remove(at: i)
+        if !source.contains(a){
+            unique.append(a)
+        }
+        source.insert(a, at: i)
+    }
+    for item in unique{
+        StringOut += "\(item) "
+    }
+    print(StringOut)
+}
+
