d50829d245429ef995c3061eaf08dc7316e2b883
Solovey42 <solovey.1999@mail.ru>
2020-10-11 05:12:33 +0700

    Completed fifth task

--- Sources/Swift_Test_app5/main.swift
+++ Sources/Swift_Test_app5/main.swift
@@ -3,3 +3,43 @@
 //
 
 import Foundation
+
+var FirstSet: Set<String> = []
+var SecondSet: Set<String> = []
+var FirstStringArray = [String]()
+var SecondStringArray = [String]()
+
+if CommandLine.arguments.count == 1 {
+    let FirstString = readLine()!
+    let SecondString = readLine()!
+    FirstStringArray = FirstString.components(separatedBy: " ")
+    SecondStringArray = SecondString.components(separatedBy: " ")
+    intersection(StringArray1: FirstStringArray, StringArray2: SecondStringArray)
+
+} else {
+    let FirsSetCount = CommandLine.arguments[1]
+    for i in 2...1+Int(FirsSetCount)! {
+        let Element = CommandLine.arguments[i]
+        FirstStringArray.append(Element)
+    }
+    let SecondSetCount = CommandLine.arguments[Int(FirsSetCount)!+2]
+    for i in 3+Int(FirsSetCount)!...2+Int(FirsSetCount)!+Int(SecondSetCount)!{
+        let Element = CommandLine.arguments[i]
+        SecondStringArray.append(Element)
+    }
+    intersection(StringArray1: FirstStringArray, StringArray2: SecondStringArray)
+
+}
+func intersection(StringArray1:[String],StringArray2:[String]){
+    for element in StringArray1{
+        FirstSet.insert(element)
+    }
+    for element in StringArray2{
+        SecondSet.insert(element)
+    }
+    print(FirstSet.intersection(SecondSet))
+}
+
+
+
+
